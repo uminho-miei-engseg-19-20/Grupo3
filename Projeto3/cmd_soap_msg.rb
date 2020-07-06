ecbc6442f5bcad769652ecf917741fe6a63922c9
AdrianaMeireles80 <adriana_1998@live.com.pt>
2020-07-06 05:21:44 +0100

    Update cmd_soap_msg.rb

--- Projeto3/cmd_soap_msg.rb
+++ Projeto3/cmd_soap_msg.rb
@@ -23,7 +23,7 @@ end
 
 
 # Função que devolve o cliente de ligação (preprod ou prod) ao servidor SOAP da CMD
-def getclient(env, timeout)
+def getclient(env)
      
     return Savon.client(wsdl: get_wsdl(env))
 end
@@ -47,9 +47,68 @@ end
 #hashtype = "SHA256"
 #hashPrefix(hashtype, hash)
 
+def getcertificate(client, args)
 
+    request_data = {
+        'applicationId' => args["applicationId"].encode("UTF-8"),
+        'userId' => args["user"]
+    }
 
+    puts request_data
 
+    return client.call(GetCertificate: request_data)
+end
 
+def ccmovelsign(client, args, hashtype)
 
+    if 'docName'.in?args
+        args["docName"] = 'docname teste'
+    end
 
+    if 'hash'.in?args
+        args["hash"] = hashlib.sha256(b'Nobody inspects the spammish repetition').digest()
+    end
+
+    args["hash"] = hashPrefix(hashtype, args["hash"])
+
+    request_data = {
+        'request' => {
+            'ApplicationId' => args["applicationId"].encode('UTF-8'),
+            'DocName' => args["docName"],
+            'Hash' => args["hash"],
+            'Pin' => args["pin"],
+            'UserId' => args["userId"]
+        }
+    }
+
+    return client.call(CCMovelSign: request_data)
+end
+
+def ccmovelmultiplesign(client, args)
+
+    request_data = {
+        'request' => {
+            'ApplicationId' => args["applicationId"].encode('UTF-8'),
+            'Pin' => args["pin"],
+            'UserId' => args["userId"]
+        },
+        'documents' => {
+            'HashStructure' => [
+                {'Hash' => hashlib.sha256(b'Nobody inspects the spammish repetition').digest(),
+                 'Name' => 'docname teste1', 'id': '1234'},
+                {'Hash' => hashlib.sha256(b'Always inspect the spammish repetition').digest(),
+                 'Name' => 'docname teste2', 'id': '1235'}
+                ]}
+    }
+    return client.call(CCMovelMultipleSign: request_data)
+end
+
+def validate_otp(client, args)
+
+    request_data = {
+        'applicationId' => args["applicationId"].encode('UTF-8'),
+        'processId' => args["ProcessId"],
+        'code' => args["OTP"],
+    }
+    return client.call(ValidateOtp: request_data)
+end
