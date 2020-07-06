e3e4a59c0afe93d6e52345f006656ca5e675952e
AdrianaMeireles80 <adriana_1998@live.com.pt>
2020-07-06 19:47:42 +0100

    Update cmd_soap_msg.rb

--- Projeto3/cmd_soap_msg.rb
+++ Projeto3/cmd_soap_msg.rb
@@ -1,11 +1,10 @@
-require 'hashlib'
+require 'hashlib'            # hash SHA256
 require 'savon'
 
 # Função para ativar o debug, permitindo mostrar mensagens enviadas e recebidas do servidor SOAP
 def debug()
-    #Activa o debug, mostrando as mensagens enviadas e recebidas do servidor SOAP.
+    
 end
-
 #debug()
 
 # Função que devolve o URL do WSDL do SCMD (preprod ou prod)
@@ -21,24 +20,29 @@ end
 #env = 1
 #get_wsdl(env)
 
-
 # Função que devolve o cliente de ligação (preprod ou prod) ao servidor SOAP da CMD
 def getclient(env)
      
-    return Savon.client(wsdl: get_wsdl(env))
+    return Savon.client(wsdl: get_wsdl(env), 
+                        namespace: "http://Ama.Authentication.Service/"
+                        #log: true,
+                        #pretty_print_xml: true,
+                        #log_level: :debug)
+                        )
 end
 
-#env = 1
+#env = 0
 #timeout = 10
-#getclient(env,timeout)
+#getclient(env)
 
 # Devolve a hash acrescentada do prefixo do tipo de hash utilizada
 def hashPrefix(hashtype, hash)
-
+    
     prefix = {
         'SHA256'=> bytes = [0x30, 0x31, 0x30, 0x0d, 0x06, 0x09, 0x60, 0x86, 0x48, 0x01,
                                    0x65, 0x03, 0x04, 0x02, 0x01, 0x05, 0x00, 0x04, 0x20]
     }
+   
     puts prefix.fetch(hashtype, 'Only SHA256 available') + hash
     return prefix.fetch(hashtype, 'Only SHA256 available') + hash
 end
@@ -47,33 +51,34 @@ end
 #hashtype = "SHA256"
 #hashPrefix(hashtype, hash)
 
+# GetCertificate(applicationId: xsd:base64Binary, userId: xsd:string)
+#                                       -> GetCertificateResult: xsd:string
 def getcertificate(client, args)
 
     request_data = {
-        'applicationId' => args["applicationId"].encode("UTF-8"),
+        'applicationId' => Base64.encode64(args["applicationId"]),
         'userId' => args["user"]
     }
 
-    puts request_data
-
-    return client.call(GetCertificate: request_data)
+    #puts client.operations
+ 
+    return client.call(:get_certificate, message: request_data)
 end
 
 def ccmovelsign(client, args, hashtype)
 
-    if 'docName'.in?args
+    if args.has_key?("docName")
         args["docName"] = 'docname teste'
     end
 
-    if 'hash'.in?args
-        args["hash"] = hashlib.sha256(b'Nobody inspects the spammish repetition').digest()
+    if args.has_key?('hash')
+        args["hash"] = Digest::SHA256.hexdigest('Nobody inspects the spammish repetition').bytes
     end
 
     args["hash"] = hashPrefix(hashtype, args["hash"])
-
     request_data = {
         'request' => {
-            'ApplicationId' => args["applicationId"].encode('UTF-8'),
+            'ApplicationId' => Base64.encode64(args["applicationId"]),
             'DocName' => args["docName"],
             'Hash' => args["hash"],
             'Pin' => args["pin"],
@@ -81,34 +86,34 @@ def ccmovelsign(client, args, hashtype)
         }
     }
 
-    return client.call(CCMovelSign: request_data)
+    return client.call(:cc_movel_sign, message: request_data)
 end
 
 def ccmovelmultiplesign(client, args)
 
     request_data = {
         'request' => {
-            'ApplicationId' => args["applicationId"].encode('UTF-8'),
+            'ApplicationId' => Base64.encode64(args["applicationId"]),
             'Pin' => args["pin"],
             'UserId' => args["userId"]
         },
         'documents' => {
             'HashStructure' => [
-                {'Hash' => hashlib.sha256(b'Nobody inspects the spammish repetition').digest(),
+                {'Hash' => Digest::SHA256.hexdigest('Nobody inspects the spammish repetition').bytes,
                  'Name' => 'docname teste1', 'id': '1234'},
-                {'Hash' => hashlib.sha256(b'Always inspect the spammish repetition').digest(),
+                {'Hash' => Digest::SHA256.hexdigest('Always inspect the spammish repetition').bytes,
                  'Name' => 'docname teste2', 'id': '1235'}
                 ]}
     }
-    return client.call(CCMovelMultipleSign: request_data)
+    return client.call(:cc_movel_multiple_sign, message: request_data)
 end
 
 def validate_otp(client, args)
 
     request_data = {
-        'applicationId' => args["applicationId"].encode('UTF-8'),
+        'applicationId' => Base64.encode64(args["applicationId"]),
         'processId' => args["ProcessId"],
         'code' => args["OTP"],
     }
-    return client.call(ValidateOtp: request_data)
+    return client.call(:validate_otp, message: request_data)
 end
